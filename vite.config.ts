d18a86a0d08fcf12fd6098a1fca7fd3f9387887e
douglaswbc <douglas.cuimar@gmail.com>
2026-01-09 13:29:30 -0300

    3 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'url';
 
 // Criar __dirname compatível com ES Modules
